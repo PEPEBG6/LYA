407a66fbb3ddbd9db06887f668b7d937bc22daae
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2023-10-11 18:09:54 -0600

    .

--- Grafo.py
+++ Grafo.py
@@ -1,40 +1,73 @@
-# Una clase para representar un objeto graph
+#Una clase para representar un objeto grafo
 class Graph:
-    # Constructor
+    #constructor
     def __init__(self, edges, n):
-        # una lista de listas para representar una lista de adyacencia
+        #Una lista de listas para representar una lista de adyacencia
         self.adjList = [[] for _ in range(n)]
+        #Agregar bordes al grafo no dirigido
         for (src, dest) in edges:
             self.adjList[src].append(dest)
             self.adjList[dest].append(src)
 
-# Función para comprobar si es seguro asignar el color 'c' al vértice 'v'
+#Función para comprobar si es seguro asignar el colo 'c' al vertice 'v'
 def isSafe(graph, color, v, c):
+    #Verifica el color de cada vertice adyacente de 'v'
     for u in graph.adjList[v]:
         if color[u] == c:
             return False
     return True
 
-def kColorable(g, color, k, v, n):
+def kColoreable(g, color, k, v, n):
+    
     if v == n:
         print([COLORS[color[v]] for v in range(n)])
         return
-
-    for c in range(1, k + 1):
+            
+    # prueba todas las combinaciones posibles de colores disponibles
+    for c in range (1, k+1):
+       
         if isSafe(g, color, v, c):
             color[v] = c
-            kColorable(g, color, k, v + 1, n)
+            
+            kColoreable(g, color, k, v+1, n)
+           
             color[v] = 0
 
-if __name__ == '__main__':
-    k = int(input("Ingrese la cantidad de colores (k): "))  # Solicitar solo el número de colores
+def ingresar_borde():
+    print("Ingrese el primer nodo (X): ")
+    a = int(input())
+    print("Ingrese el segundo nodo (Y): ")
+    b = int(input())
+    return (a, b)
 
-    # Lista de bordes de graph según el diagrama anterior
-    edges = [(0, 1), (0, 3), (1, 2), (1, 3), (1, 4), (4, 5), (4, 6), (5, 3)]
 
-    COLORS = ['Azul', 'Azul Claro', 'Verde', 'Verde','Rojo','Negro','Violeta','Morado', ]
 
-    n = 6
-    g = Graph(edges, n)
+if __name__ =='__main__':
+    
+    edges = []
+
+    print("Cuantos colores: ")
+    k = int(input())
+
+    print("Cuantos nodos o vértices: ")
+    n = int(input())
+
+    print("Cuantas aristas: ")
+    v = int(input())
+
+    edges = []
+
+    for i in range(v):
+        edges.append(ingresar_borde())
+
+    #Lista de colores
+    COLORS = ['Azul', 'Celeste', 'Dorado', 'Café', 'Naranja', 'Verde', 'Blanco', 'Gris', 'Negro', 'Morado']
+    
+    #Crear un grafo a partir de una lista de bordes
+    g = Graph(edges, n) 
+    
     color = [None] * n
-    kColorable(g, color, k, 0, n)
+
+    print("Matriz de colores: ")
+    #Imprime todas las k-configuraciones coloreables del graph
+    kColoreable(g, color, k, 0, n)
